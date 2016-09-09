61ac96893dfed78a37cb0010213ac1395ffa0d96
Ebad Rahman <rahmanek@gmail.com>
2016-09-09 00:42:42 -0400

    updated config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 export default {
-	apiHost: "http://localhost:3010" ,
-	webHost: "http://localhost:3000/",
+	apiHost: "http://api.flectino.com" ,
+	webHost: "http://www.flectino.com",
 	gatewayKey: "AUB5jCkdq3b7kV9DTTdiQllORv5"
 }
