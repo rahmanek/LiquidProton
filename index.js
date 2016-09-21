ec58322c8874c88544947d1ac12ed40b17de5295
Ebad Rahman <rahmanek@gmail.com>
2016-09-20 23:00:41 -0400

    Heroku fix

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app = express();
 var port = process.env.PORT || 3001;
 var path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
 });
 
 var listener = app.listen(port, function() {
