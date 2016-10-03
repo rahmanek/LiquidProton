51b4e2e250e09d928a3bc3aedd079279da324a6e
Ebad Rahman <rahmanek@gmail.com>
2016-10-03 17:44:04 -0400

    Fix

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 
+var environment = "development";
+
 export default {
+	environment: environment,
 	apiHost: (function(){
-		if(process.env.NODE_ENV == "production") return "http://apitest.flectino.com";
+		if(environment == "production") return "http://apitest.flectino.com";
 		else return "http://localhost:3010";
 	}()),
 	webHost: (function(){
-		if(process.env.NODE_ENV == "production") return "http://webtest.flectino.com";
+		if(environment == "production") return "http://webtest.flectino.com";
 		else return "http://localhost:3000";
 	}()),
 	gatewayKey: "AUB5jCkdq3b7kV9DTTdiQllORv5"
